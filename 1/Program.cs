6c62d010362c32518968558a8be49eeaf6376c97
emanu <emanu@EMANUEL_RAMREZ>
2026-04-10 10:36:22 -0600

    Agregar archivos de proyecto.

---
+++ 1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Solicitar al usuario la cantidad n de números enteros y 
+//almacenarlos en un arreglo. Luego, recorrer el arreglo para calcular 
+//el promedio general. Después, mostrar en pantalla el promedio y 
+//listar únicamente los valores que están por encima del promedio. 
+//Finalmente, indicar cuántos números cumplen esa condición.
